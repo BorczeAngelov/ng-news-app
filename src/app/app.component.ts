af558d2047752e5d8570272f485025a7cb442d2d
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-02-19 14:45:27 +0100

    display data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,45 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NewsApiService } from './service/news-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
   title = 'ng-news-app';
 
+  public selectedNewsChannel: string = "Top 10 Trending News!";
+  public sources: any = [];
+  public articles: any = [];
 
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
 
-  constructor(private observer: BreakpointObserver, private cd : ChangeDetectorRef) { }
+  constructor(
+    private observer: BreakpointObserver,
+    private cd: ChangeDetectorRef,
+    private newsApi: NewsApiService) { }
+
+
+  ngOnInit(): void {
+
+    this.newsApi.initArticles()
+      .subscribe((response: any) => {
+
+        console.log(response);
+        this.articles = response.articles;
+      });
+
+    this.newsApi.initSources()
+      .subscribe((response: any) => {
+
+        console.log(response);
+        this.sources = response.sources;
+      });
+
+  }
 
 
   ngAfterViewInit(): void {
@@ -23,7 +49,7 @@ export class AppComponent implements AfterViewInit {
       .subscribe(response => {
         if (response?.matches) {
           console.log("screen width is lower than 800px");
-          
+
           this.sideNav.mode = "over";
           this.sideNav.close();
         }
@@ -32,8 +58,21 @@ export class AppComponent implements AfterViewInit {
           this.sideNav.open();
         }
       });
-      
+
     this.cd.detectChanges();
+  }
+
+  searchSource(source: any) {
+    console.log(source);
+    
+    this.newsApi.getArticlesByID(source.id)
+      .subscribe((response: any) => {
+
+        console.log(response);
+        this.selectedNewsChannel = source.name;
+        this.articles = response.articles;
+
+      })
   }
 
 }
